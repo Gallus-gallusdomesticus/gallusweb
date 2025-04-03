f61943a17b1a529cf5d4c1fd2ca080ef85db8fb9
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2025-04-03 18:35:29 +1100

    Add text to html node

---
+++ src/texttohtmlnode.py
@@ -0,0 +1,24 @@
+from htmlnode import HTMLNode, LeafNode
+from textnode import TextNode, TextType
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type==TextType.NORMAL:
+        return LeafNode(tag=None, value=text_node.text)
+
+    if text_node.text_type==TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+
+    if text_node.text_type==TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+
+    if text_node.text_type==TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+        
+    if text_node.text_type==TextType.LINKS:
+        return LeafNode(tag="a", value=text_node.text, props={"href":text_node.url})
+
+    if text_node.text_type==TextType.IMAGES:
+        return LeafNode(tag="img", value="", props={"src":text_node.url, "alt":text_node.text})
+    
+    raise Exception("TextType does not exist")
