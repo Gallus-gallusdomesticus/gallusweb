345088177e39356e3b9da3fd30983289030e4d52
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2025-04-06 15:27:34 +1000

    Add split for images and links

--- src/splitnodedelimiter.py
+++ src/splitnodedelimiter.py
@@ -20,3 +20,57 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     new_nodes.append(TextNode(word_node_list[i], text_type))
                 i+=1
     return new_nodes
+
+
+
+#Split nodes for images and links
+
+from extractlinks import extract_markdown_images, extract_markdown_links
+
+def split_nodes_image(old_nodes):
+    new_nodes=[]
+    
+    for nodes in old_nodes: #each node in old nodes
+        
+        extracted=extract_markdown_images(nodes.text) #extract the markdown in the node
+
+        if extracted == []: #if there is nothing to extract just return as it is
+            new_nodes.extend([nodes])
+
+        else:
+            remaining=nodes.text
+            for extract in extracted: #for each list that get extracted
+                splitted_nodes = remaining.split(f"![{extract[0]}]({extract[1]})",1) #split the nodes and remove the extracted once
+                if splitted_nodes[0]: #if there is something in beginning of the splitted nodes
+                    new_nodes.extend([TextNode(splitted_nodes[0],TextType.TEXT)])
+                new_nodes.extend([TextNode(extract[0], TextType.IMAGES, extract[1])]) #append the extracted to new_nodes    
+                remaining=splitted_nodes[1] #update the remaining to the remainder
+            if remaining: #if there is remainder on the end
+                new_nodes.extend([TextNode(remaining, TextType.TEXT)])
+
+    return new_nodes
+
+def split_nodes_links(old_nodes):
+    new_nodes=[]
+    
+    for nodes in old_nodes: #each node in old nodes
+        
+        extracted=extract_markdown_links(nodes.text) #extract the markdown in the node
+
+        if extracted == []: #if there is nothing to extract just return as it is
+            new_nodes.extend([nodes])
+
+        else:
+            remaining=nodes.text
+            for extract in extracted: #for each list that get extracted
+                splitted_nodes = remaining.split(f"[{extract[0]}]({extract[1]})",1) #split the nodes and remove the extracted once
+                if splitted_nodes[0]: #if there is something in beginning of the splitted nodes
+                    new_nodes.extend([TextNode(splitted_nodes[0],TextType.TEXT)])
+                new_nodes.extend([TextNode(extract[0], TextType.LINKS, extract[1])]) #append the extracted to new_nodes    
+                remaining=splitted_nodes[1] #update the remaining to the remainder
+            if remaining: #if there is remainder on the end
+                new_nodes.extend([TextNode(remaining, TextType.TEXT)])
+
+    return new_nodes
+
+
--- src/test_splitnodedelimiter.py
+++ src/test_splitnodedelimiter.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from splitnodedelimiter import split_nodes_delimiter
+from splitnodedelimiter import split_nodes_delimiter, split_nodes_image, split_nodes_links
 
 
 class TestSplitNodeDelimiter(unittest.TestCase):
@@ -63,6 +63,74 @@ class TestSplitNodeDelimiter(unittest.TestCase):
             split_nodes_delimiter([node], "`", TextType.CODE)
     
 
+
+#Test split node image
+
+class TestSplitNodeImage(unittest.TestCase):
+    def test_sample(self):
+        node = TextNode(
+        "This is text with a image ![image1](https://www.boot.dev/image) and ![image2](https://www.youtube.com/@bootdotdev/image)",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        expected= [
+        TextNode("This is text with a image ", TextType.TEXT),
+        TextNode("image1", TextType.IMAGES, "https://www.boot.dev/image"),
+        TextNode(" and ", TextType.TEXT),
+        TextNode("image2", TextType.IMAGES, "https://www.youtube.com/@bootdotdev/image"),]
+
+        self.assertEqual(len(new_nodes), len(expected))
+        for i in range(len(new_nodes)):
+            self.assertEqual(new_nodes[i].text, expected[i].text)
+            self.assertEqual(new_nodes[i].text_type, expected[i].text_type)
+
+    def test_mixed(self):
+        node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and image ![image2](https://www.youtube.com/@bootdotdev/image)",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        expected= [
+        TextNode("This is text with a link [to boot dev](https://www.boot.dev) and image ", TextType.TEXT),
+        TextNode("image2", TextType.IMAGES, "https://www.youtube.com/@bootdotdev/image"),
+        ]
+
+
+
+class TestSplitNodeLink(unittest.TestCase):
+    def test_sample(self):
+        node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_links([node])
+        expected= [
+        TextNode("This is text with a link ", TextType.TEXT),
+        TextNode("to boot dev", TextType.LINKS, "https://www.boot.dev"),
+        TextNode(" and ", TextType.TEXT),
+        TextNode("to youtube", TextType.LINKS, "https://www.youtube.com/@bootdotdev")]
+
+        self.assertEqual(len(new_nodes), len(expected))
+        for i in range(len(new_nodes)):
+            self.assertEqual(new_nodes[i].text, expected[i].text)
+            self.assertEqual(new_nodes[i].text_type, expected[i].text_type)
+
+    def test_mixed(self):
+        node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and image ![image2](https://www.youtube.com/@bootdotdev/image)",
+        TextType.TEXT,
+        )
+        new_nodes = split_nodes_links([node])
+        expected= [
+        TextNode("This is text with a link ", TextType.TEXT),
+        TextNode("to boot dev", TextType.LINKS, "https://www.boot.dev"),
+        TextNode(" and image ![image2](https://www.youtube.com/@bootdotdev/image)", TextType.TEXT),
+        ]
+
+        self.assertEqual(len(new_nodes), len(expected))
+        for i in range(len(new_nodes)):
+            self.assertEqual(new_nodes[i].text, expected[i].text)
+            self.assertEqual(new_nodes[i].text_type, expected[i].text_type)
 
 
 if __name__ == "__main__":
