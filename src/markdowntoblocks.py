ebaa51d7627ef01178659c5fb0f87cd988a02a82
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2025-04-07 19:54:48 +1000

    Add markdown to blocks function

---
+++ src/markdowntoblocks.py
@@ -0,0 +1,19 @@
+def markdown_to_blocks(markdown):
+    md_split=markdown.split("\n\n")
+
+    for i in range(len(md_split)):
+        md_split[i]=md_split[i].strip()
+    
+    md_split=list(filter(None, md_split))
+
+    return md_split
+
+print(markdown_to_blocks("""
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""))
